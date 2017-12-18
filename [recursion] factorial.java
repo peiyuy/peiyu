e5bafecbce248ba5206b0bdffaf1bc6e17944276
peiyuy <peiyuyi1103@163.com>
2017-12-18 01:18:48 -0800

    Create [recursion] factorial.java

---
+++ [recursion] factorial.java
@@ -0,0 +1,9 @@
+public static int factorial(int n) {
+    if (n < 0) {
+        throw new IllegalArgumentException();
+    } else if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
